5320117002face3788b771c7915bbdd3f404582f
webbestmaster <web.best.master@gmail.com>
2016-04-10 23:56:18 +0300

    device - refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,44 @@
-/*jslint white: true, nomen: true */
-(function () {
+'use strict';
 
-	var gulp = require('gulp');
+var gulp = require('gulp'),
+	// clean = require('gulp-rimraf'),
+	minifyHTML = require('gulp-minify-html'),
+	minifyCss = require('gulp-minify-css'),
+	es6Import = require('gulp-es6-import');
 
-}());
+gulp.task('default', function () {
+	return gulp.start('html', 'css', 'js');
+});
+
+gulp.task('watch', ['html', 'css', 'js'], function () {
+	gulp.watch('./www/*.html', ['html']);
+	gulp.watch('./www/css/**/*', ['css']);
+	gulp.watch('./www/js/**/*', ['js']);
+});
+
+gulp.task('html', function () {
+	return gulp.src('./www/*.html')
+		.pipe(minifyHTML({
+			conditionals: true,
+			spare: true
+		}))
+		.pipe(gulp.dest('./dist/www/'));
+});
+
+gulp.task('css', function () {
+	return gulp.src('./www/css/main.css')
+		.pipe(minifyCss())
+		.pipe(gulp.dest('./dist/www/css'));
+});
+
+
+// JS
+gulp.task('js', function () {
+	return gulp.start('collect-js');
+});
+
+gulp.task('collect-js', function () {
+	return gulp.src('./www/js/main.js')
+		.pipe(es6Import())
+		.pipe(gulp.dest('./dist/www/js/'));
+});
---
+++ www/js/main.js
@@ -0,0 +1,21 @@
+// init mediator
+import mediator from 'services/mediator';
+
+// init libs
+import PIXI from 'lib/pixi';
+
+// init services
+import log from 'services/log'; // remove
+import device from 'services/device'; // remove
+
+function main() {
+	device.initialize();
+
+}
+
+var win = window.addEventListener('load', main, false);
+
+
+
+
+
