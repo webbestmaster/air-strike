17d242a6031022ee6f392514a1e8db2dd2bc64ae
webbestmaster <web.best.master@gmail.com>
2016-05-06 16:20:33 +0300

    aircraft - add sprite sheet.

---
+++ graphic-raw/aircraft/sprites.js
@@ -0,0 +1,45 @@
+{
+"frames": {
+	"aircraft-0.png": {
+		"frame": {"x":0, "y":62, "w":47, "h":28},
+		"spriteSourceSize": {"x":0,"y":0,"w":47,"h":28},
+		"sourceSize": {"w":47,"h":28}
+	},
+	"aircraft-1.png": {
+		"frame": {"x":0, "y":0, "w":47, "h":29},
+		"spriteSourceSize": {"x":0,"y":0,"w":47,"h":29},
+		"sourceSize": {"w":47,"h":29}
+	},
+	"aircraft-2.png": {
+		"frame": {"x":0, "y":30, "w":47, "h":31},
+		"spriteSourceSize": {"x":0,"y":0,"w":47,"h":31},
+		"sourceSize": {"w":47,"h":31}
+	},
+	"aircraft-3.png": {
+		"frame": {"x":0, "y":91, "w":46, "h":32},
+		"spriteSourceSize": {"x":0,"y":0,"w":46,"h":32},
+		"sourceSize": {"w":46,"h":32}
+	},
+	"aircraft-4.png": {
+		"frame": {"x":48, "y":0, "w":45, "h":33},
+		"spriteSourceSize": {"x":0,"y":0,"w":45,"h":33},
+		"sourceSize": {"w":45,"h":33}
+	},
+	"aircraft-5.png": {
+		"frame": {"x":48, "y":34, "w":44, "h":34},
+		"spriteSourceSize": {"x":0,"y":0,"w":44,"h":34},
+		"sourceSize": {"w":44,"h":34}
+	},
+	"aircraft-6.png": {
+		"frame": {"x":48, "y":69, "w":43, "h":35},
+		"spriteSourceSize": {"x":0,"y":0,"w":43,"h":35},
+		"sourceSize": {"w":43,"h":35}
+	}
+
+},
+"meta": {
+	"image": "sprites.png",
+	"size": {"w": 94, "h": 124},
+	"scale": "1"
+}
+}
