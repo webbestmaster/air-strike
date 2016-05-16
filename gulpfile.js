33d8676a1d9e4753e3cdeeb2046669b8301eeeb9
webbestmaster <web.best.master@gmail.com>
2016-05-16 13:30:34 +0300

    broke gulp :)

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,26 @@ var gulp = require('gulp'),
 	minifyHTML = require('gulp-minify-html'),
 	minifyCss = require('gulp-minify-css'),
 	rjs = require('gulp-requirejs'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+	fs = require('fs'),
+	requirePaths;
+
+(function () {
+
+	requirePaths =
+		JSON.parse(
+			fs.readFileSync('www/js/require-config.js')
+			.toString()
+			.replace(/\/\/[\s\S]+?\n/g, '') 	// remove comments
+			.replace(/\'/g, '\"') 				// prop: 'value' to prop : "value"
+			.replace(/(\S+)\:/g, '\"$1\":') 	// prop: "value" to "prop" : "value"
+			.match(/\{[\s\S]+\}/)
+			.toString()
+	);
+
+	console.log(requirePaths);
+
+}());
 
 gulp.task('default', function () {
 	return gulp.start('copy-assets', 'html', 'css', 'js');
@@ -51,63 +70,7 @@ gulp.task('collect-js', function () {
 			baseUrl: './www/js/',
 			out: 'main.js',
 			// copy paths from main.js
-			paths: {
-				// init service
-				log: 'services/log',
-				mediator: 'services/mediator',
-				// init libs
-				Deferred: 'lib/deferred',
-				fontLoader: 'lib/font-loader',
-				PIXI: 'lib/pixi',
-				util: 'lib/util',
-				//EasePack: 'lib/EasePack',
-				TweenMax: 'lib/TweenMax',
-				FPSMeter: 'lib/fpsmeter',
-				// init service
-				device: 'services/device',
-				deviceKeys: 'services/device-keys',
-				// core
-				// requireAsset: 'services/require-asset',
-				renderer: 'core/renderer',
-				rendererKeys: 'core/renderer-keys',
-				textureMaster: 'core/texture-master',
-				textureSources: 'core/texture-sources',
-				DisplayObject: 'core/display-object',
-				BaseView: 'core/base-view',
-				baseViewKeys: 'core/base-view-keys',
-				Button: 'core/button',
-				loader: 'core/loader',
-				camera: 'core/camera',
-				cameraKeys: 'core/camera-keys',
-				uiManager: 'core/ui-manager',
-				uiManagerKeys: 'core/ui-manager-keys',
-				CollisionManager: 'core/collision-manager',
-				collisionManagerKeys: 'core/collision-manager-keys',
-				// views
-				TitleView: 'view/title/view',
-				SettingView: 'view/setting/view',
-
-				// game
-				GameModel: 'game/game-model',
-				GameView: 'game/game-view',
-				gameKeys: 'game/game-keys',
-				gameConfig: 'game/game-config',
-				gameState: 'game/game-state',
-				Factory: 'factory/factory',
-				factoryKeys: 'factory/factory-keys',
-				objectKeys: 'factory/object-keys',
-				constructorMap: 'factory/constructor-map',
-
-				// game objects
-				GameObject: 'objects/game-object',
-				gameObjectKeys: 'objects/game-object-keys',
-				// gameObjectHelper: 'objects/game-object-helper',
-				Aircraft: 'objects/aircraft',
-				Bullet: 'objects/bullet',
-				JuniorMissile: 'objects/junior-missile',
-				Cross: 'objects/cross'
-
-			}
+			paths: requirePaths
 		}))
 		.pipe(gulp.dest('./dist/www/js/'));
 
